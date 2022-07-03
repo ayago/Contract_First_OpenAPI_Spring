56d156c2e48e0a9af361618ce0a06e6c41d11afd
Adrian Yago <adriancyago@gmail.com>
2022-07-03 17:29:48 +0800

    Initialized project structure

---
+++ src/main/java/adrianyago/examples/spring/openapi/generated/api/SpringOpenapiGeneratedApiApplication.java
@@ -0,0 +1,13 @@
+package adrianyago.examples.spring.openapi.generated.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenapiGeneratedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenapiGeneratedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adrianyago/examples/spring/openapi/generated/api/SpringOpenapiGeneratedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package adrianyago.examples.spring.openapi.generated.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenapiGeneratedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
